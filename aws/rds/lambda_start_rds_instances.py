16b759f21cfffcafdcd9e1cade9dc863eae52a16
Bruno Zandonai <bruno.zandonai@gmail.com>
2024-03-18 11:39:05 -0300

    chore: lambda scripts for RDS instances

---
+++ aws/rds/lambda_start_rds_instances.py
@@ -0,0 +1,23 @@
+# author: Bruno Romero Costa Zandonai
+# version: 0.1.2
+# date: 23/02/2024
+# description: start RDS instances, passed as parameter (from parameter store, or other source)
+# use case: import script on AWS Lambda, and put in AWS EventBridge a rule do execute/schedule the Lambda call
+# impacted services: RDS, Lambda, EventBridge
+# possible improvments: collect RDS instances identifiers using boto3 RDS client
+
+import boto3
+
+REGION = 'AWS_REGION'
+AWS_ACCESS_KEY = "AWS_ACCESS_KEY_HERE"
+AWS_SECRET_KEY = "AWS_SECRET_KEY_HERE"
+
+identifier = 'DB_INSTANCE_IDENTIFIER'
+rds = boto3.client('rds', 
+    region_name=REGION,
+    aws_access_key_id = AWS_ACCESS_KEY,
+    aws_secret_access_key = AWS_ACCESS_KEY
+)
+
+def lambda_handler(event, context):
+    rds.start_db_instance(DBInstanceIdentifier=identifier)
